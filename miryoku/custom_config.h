8efed4339dc282f566f5b59682fed946c353fd41
hugo.nogard@posteo.net <hugo.nogard@posteo.net>
2023-02-03 13:41:59 +0100

    add layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,8 +2,8 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_LAYER_TWM \
-&kp A,             &kp Z,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LGUI, Q),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, M),     \
+&kp LG(LS(N1)),             &kp LG(LS(N2)),             &kp LG(LS(N3)),             &kp LG(LS(N4)),             &kp LG(LS(N5)),             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+&kp LG(N1),     &kp LG(N2),     &kp LG(N3),    &kp LG(N4),    &kp LG(N5),             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, M),     \
 U_LT(U_BUTTON, W), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp COMMA,         &kp DOT,           U_MT(RALT, SLASH), U_LT(U_BUTTON, SQT),\
 U_NP,              U_NP,              U_LT(U_FUN, DEL),  U_LT(U_NUM, BSPC), U_LT(U_SYM, RET),  U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPACE),U_LT(U_MEDIA, ESC),U_NP,              U_NP
 
