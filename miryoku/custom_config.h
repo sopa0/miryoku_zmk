da9a950647b9aa35200c98cc297d0d0f6115d321
hugo.nogard@posteo.net <hugo.nogard@posteo.net>
2023-02-03 12:40:13 +0100

    fix

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,33 +1,3 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define TWM \
-&kg LG(LS(N1)),    &kp LG(LS(N2)),    &kp LG(LS(N3)),    &kp LG(LS(N4)),    &kp LG(LS(N5)),    U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-&kp LG(N1),        &kp LG(N2),        &kp LG(N3),        &kp LG(N4),        &kg LG(N5),        U_NU,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
-
-#define MIRYOKU_LAYER_LIST \
-MIRYOKU_X(BASE,   "Base") \
-MIRYOKU_X(EXTRA,  "Extra") \
-MIRYOKU_X(TAP,    "Tap") \
-MIRYOKU_X(BUTTON, "Button") \
-MIRYOKU_X(NAV,    "Nav") \
-MIRYOKU_X(MOUSE,  "Mouse") \
-MIRYOKU_X(MEDIA,  "Media") \
-MIRYOKU_X(NUM,    "Num") \
-MIRYOKU_X(SYM,    "Sym") \
-MIRYOKU_X(FUN,    "Fun") \
-MIRYOKU_X(TWM,    "Twm")
-
-#define U_BASE   0
-#define U_EXTRA  1
-#define U_TAP    2
-#define U_BUTTON 3
-#define U_NAV    4
-#define U_MOUSE  5
-#define U_MEDIA  6
-#define U_NUM    7
-#define U_SYM    8
-#define U_FUN    9
-#define U_TWM    10
